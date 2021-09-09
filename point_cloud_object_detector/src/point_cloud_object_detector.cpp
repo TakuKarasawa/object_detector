eed3a3430c8e834c849786e3e49f8e068264b815
TakuKarasawa <taku-ka@outlook.com>
2021-09-09 21:35:59 +0900

    update

--- point_cloud_object_detector/src/point_cloud_object_detector.cpp
+++ point_cloud_object_detector/src/point_cloud_object_detector.cpp
@@ -78,40 +78,54 @@ void PointCloudObjectDetector::bbox_process_2()
 {
     for(const auto &b : bboxes.bounding_boxes){
         std::cout << b.Class << std::endl;
-        std::vector<float> points;
-        std::vector<std::vector<float>> z_points(cloud->height,std::vector<float>());
-        std::vector<float> z_value;
+        std::vector<pcl::PointXYZRGB> points;
+        std::vector<std::vector<pcl::PointXYZRGB>> rearranged_points(cloud->height,std::vector<pcl::PointXYZRGB>());
+        std::vector<pcl::PointXYZRGB> values;
         
-        for(const auto &p : cloud->points) points.push_back(p.z);
 
+        for(const auto &p : cloud->points) points.push_back(p);
+        
         if(points.size() == cloud->width*cloud->height){
             for(int i = 0; i < cloud->height; i++){
                 for(int j = 0; j < cloud->width; j++){
-                    z_points.at(i).push_back(points.at(i*cloud->width+j));
+                    rearranged_points.at(i).push_back(points.at(i*cloud->width+j));
                 }
             }
 
+            
             if(!(b.xmin == 0 && b.xmax == 0)){
                 for(int x = b.xmin; x <= b.xmax; x++){
                     for(int y = b.ymin; y <= b.ymax; y++){
-                        z_value.push_back(z_points.at(y).at(x));
+                        values.push_back(rearranged_points.at(y).at(x));
                     }
                 }
 
-                double d = 0.0;
+                double sum_x = 0.0;
+                double sum_y = 0.0;
+                double sum_z = 0.0;
                 int finite_count = 0;
-                for(int i = 0; i < z_value.size(); i++){
-                    if(isfinite(z_value.at(i))){
-                        d += z_value.at(i);
+                for(const auto &value : values){
+                    if(isfinite(value.x) && isfinite(value.y) && isfinite(value.z)){
+                        sum_x += value.x;
+                        sum_y += value.y;
+                        sum_z += value.z;
                         finite_count ++;
                     }
                 }
                 
-                d /= (double)finite_count;
-                std::cout << d << std::endl;
+                sum_x /= (double)finite_count;
+                sum_y /= (double)finite_count;
+                sum_z /= (double)finite_count;
 
-                double dist = *min_element(z_value.begin(),z_value.end());
-                std::cout << dist << std::endl;
+                std::cout << "x: " << sum_x << std::endl;
+                std::cout << "y: " << sum_y << std::endl;
+                std::cout << "z: " << sum_z << std::endl;
+
+                double d = sqrt(pow(sum_x,2)+pow(sum_z,2));
+                double theta = atan2(sum_z,sum_x) - M_PI/2;
+                std::cout << "d: " << d << std::endl;
+                std::cout << "theta: " << theta << std::endl;
+
             }
         }
     }
@@ -121,7 +135,7 @@ void PointCloudObjectDetector::process()
 {
     ros::Rate rate(1);
     while(ros::ok()){
-        if(has_received_bbox && has_received_pcl2) bbox_process();
+        if(has_received_bbox && has_received_pcl2) bbox_process_2();
         ros::spinOnce();
         rate.sleep();
     }   
